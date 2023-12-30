1757278140be2dbc652e3125ed31d2122b9d317a
M4teoo <mateosolino1@gmail.com>
2023-12-30 01:38:43 +0100

    fix deploy bugs 2

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,13 @@
-import { useState } from "preact/hooks"
+// import { useState } from "preact/hooks"
 
-export function Counter() {
-    const [counter, setCounter] = useState(0)
+// export function Counter() {
+//     const [counter, setCounter] = useState(0)
 
-    return (
-        <>
-            <span class="text-yellow-300 text-xl mr-4">{counter}</span>
-            <button class="border px-4 py-2 text-xl" onClick={() => setCounter(counter => counter + 1)}>+</button>
-            <button class="border px-4 py-2 text-xl" onClick={() => setCounter(counter => counter - 1)}>-</button>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <span class="text-yellow-300 text-xl mr-4">{counter}</span>
+//             <button class="border px-4 py-2 text-xl" onClick={() => setCounter(counter => counter + 1)}>+</button>
+//             <button class="border px-4 py-2 text-xl" onClick={() => setCounter(counter => counter - 1)}>-</button>
+//         </>
+//     )
+// }
